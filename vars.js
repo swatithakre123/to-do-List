2865579723bb41dab929912f18fe7cfddd01d95d
swatithakre123 <swatithakre3024@gmail.com>
2024-10-18 11:58:51 +0530

    Add files via upload

---
+++ vars.js
@@ -0,0 +1,21 @@
+export default {
+  colors: {
+    bg: `#403F4D`,
+    green: `#62DCA5`,
+    yellow: `#F7F879`,
+    red: `#E1675A`,
+    blue: `#16A9FF`,
+    inputColor: `#2E2D33`,
+    itemColor: `#4E4D5C`,
+    fontColor: `#FBFAFB`,
+    gray: `#D0D4D7`
+  },
+  bigFontSize: `64px`,
+  headingFontSize: `24px`,
+  itemFontSize: `20px`
+}
+
+export const ENV = {
+  API_KEY: `keyIye3zskPSBMQ6Q`,
+  endpoint: `https://api.airtable.com/v0/appbEcFG0Ek5EXr3M/todometerv1`
+}
